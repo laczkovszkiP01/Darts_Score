c500f0cee405d42820839a3bfbcd5793788d8841
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-04-02 16:54:19 +0200

    declared main.jsx routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import GameMenu from './pages/GameMenu/GameMenu.jsx'
+import Game from './pages/Game/Game.jsx'
+import Login from './pages/Login/Login.jsx'
+import Register from './pages/Register/Register.jsx'
+import Profile from './pages/Profile/Profile.jsx'
+import MatchDetails from './pages/MatchDetails/MatchDetails.jsx'
+import NotFound from './pages/NotFound/NotFound.jsx'
+import Admin from './pages/Admin/Admin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gameMenu" element={<GameMenu />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/match/:id" element={<MatchDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
