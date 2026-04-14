985741676d7b81ce21301b929a4f2bcf6ee5f6ac
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-04-14 19:53:35 +0200

    bugfix DartPartsSection component

--- src/components/DartPartsSection/DartPartsSection.jsx
+++ src/components/DartPartsSection/DartPartsSection.jsx
@@ -11,25 +11,25 @@ function DartPartsSection() {
             <img src={dartImage} alt="Darts nyíl" />
           </div>
           <div className={style.textContainer}>
-            <div className={style.partItem}>
+            <div className={style.dartsnyilreszek}>
               <h4>Nyél / Shaft</h4>
               <p>
                 A nyíl hátsó része, amely a tollat tartja. Ez befolyásolja az egyensúlyt és a repülést.
               </p>
             </div>
-            <div className={style.partItem}>
+            <div className={style.dartsnyilreszek}>
               <h4>Főtő / Barrel</h4>
               <p>
                 A nyíl középső, megfogható része. Ez adja a súly nagy részét, és a fogás érzete itt a legfontosabb.
               </p>
             </div>
-            <div className={style.partItem}>
+            <div className={style.dartsnyilreszek}>
               <h4>Csúcs / Tip</h4>
               <p>
                 Ez az a rész, amely a táblába érkezik. Lehet acélhegyű vagy soft tip, attól függően, milyen táblán játszol.
               </p>
             </div>
-            <div className={style.partItem}>
+            <div className={style.dartsnyilreszek}>
               <h4>Toll / Flight</h4>
               <p>
                 A hátsó stabilizáló elem, amely segít a nyílnak egyenesen repülni és megtartani a megfelelő irányt.
