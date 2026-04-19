bc9951d05d0f899ea6788bf2419c983d1e86baa1
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-04-19 17:35:06 +0200

    declared gameLogic.js

---
+++ src/pages/Game/gameLogic.js
@@ -0,0 +1,38 @@
+export const evaluateRound = ({ initialScore, throwsList, outMode }) => {
+  let remaining = initialScore;
+
+  for (let i = 0; i < throwsList.length; i += 1) {
+    const currentThrow = throwsList[i];
+    remaining -= currentThrow.total;
+
+    if (remaining < 0) {
+      return {
+        bust: true,
+        win: false,
+        remaining: initialScore,
+      };
+    }
+
+    if (remaining === 0) {
+      if (outMode === 'double_out' && currentThrow.multiplier !== 2) {
+        return {
+          bust: true,
+          win: false,
+          remaining: initialScore,
+        };
+      }
+
+      return {
+        bust: false,
+        win: true,
+        remaining: 0,
+      };
+    }
+  }
+
+  return {
+    bust: false,
+    win: false,
+    remaining,
+  };
+};
