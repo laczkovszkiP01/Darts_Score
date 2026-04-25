2c77573195b297e3b04bfcadd05ec409b7d51573
laczkovszkiP01 <21c_laczkovszkip@nyirszikszi.hu>
2026-04-25 17:36:34 +0200

    updated contentboxType2 component

--- src/components/ContentBoxType2/ContentBoxType2.jsx
+++ src/components/ContentBoxType2/ContentBoxType2.jsx
@@ -5,16 +5,24 @@ function ContentBoxType2({cim,bal,jobb,balH4,jobbH4,jobbKep,balKep,})
 {
     return (
         <div className={styles.container}>
+
             <h2>{cim}</h2>
+
             <div className={styles.row}>
+
                 <div className={styles.left}>
+
                     <h4>{balH4}</h4>
                     <p>{bal}</p>
                     <img src={balKep} alt="Darts nyíl" />
+
                 </div>
+
                 <div className={styles.right}>
+
                     <h4>{jobbH4}</h4>
                     <p>{jobb}</p>
+                    
                     <img src={jobbKep} alt="Darts tábla" />
                 </div>
             </div>
